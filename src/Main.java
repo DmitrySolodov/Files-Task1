49a0ee457e27f26c87a1bc25041073c82f933684
DmitrySolodov <solodov.d.s@yandex.ru>
2022-04-19 13:45:48 +0700

    Main.java changed, add cycles, methods and lists

--- src/Main.java
+++ src/Main.java
@@ -1,82 +1,60 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void addDirectory (File file, StringBuilder log) {
-        if (file.mkdir()) {
-            log.append("Каталог " + file.getAbsolutePath() + " успешно создан\n");
-        } else {
-            log.append("Ошибка создания директории " + file.getAbsolutePath() + "\n");
+    public static void addDirectory (List<File> folderList, StringBuilder log) {
+        for (File catalog : folderList) {
+            if (catalog.mkdirs()) {
+                log.append("Каталог " + catalog.getAbsolutePath() + " успешно создан\n");
+            } else {
+                log.append("Ошибка создания директории " + catalog.getAbsolutePath() + "\n");
+            }
         }
     }
 
-    public static void addFile (File file, StringBuilder log) throws IOException {
-        if (file.createNewFile()) {
-            log.append("Файл в каталоге " + file.getAbsolutePath() + " успешно создан\n");
-        } else {
-            log.append("Ошибка создания файла " + file.getAbsolutePath() + "\n");
+    public static void addFile (List<File> fileslist, StringBuilder log) {
+        for (File files : fileslist) {
+            try {
+                if (files.createNewFile()) {
+                    log.append("Файл в каталоге " + files.getAbsolutePath() + " успешно создан\n");
+                } else {
+                    log.append("Ошибка создания файла " + files.getAbsolutePath() + "\n");
+                }
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
-    public static void main(String[] args) {
-        StringBuilder log = new StringBuilder();
-        //Первый этап
-        File srsDirectory = new File("E://Games/src");
-        addDirectory(srsDirectory, log);
-
-        File resDirectory = new File("E://Games/res");
-        addDirectory(resDirectory, log);
-
-        File savegamesDirectory = new File("E://Games/savegames");
-        addDirectory(savegamesDirectory, log);
-
-        File tempDirectory = new File("E://Games/temp");
-        addDirectory(tempDirectory, log);
-
-        //Второй этап
-        File mainDirectory = new File(srsDirectory, "main");
-        addDirectory(mainDirectory, log);
-
-        File testDirectory = new File(srsDirectory, "test");
-        addDirectory(testDirectory, log);
-
-        //Третий этап
-        File mainJava = new File(mainDirectory, "Main.java");
-        try {
-            addFile(mainJava, log);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        File utilsJava = new File(mainDirectory, "Utils.java");
-        try {
-            addFile(utilsJava, log);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        //Четвертый этап
-        File drawablesDirectory = new File(resDirectory, "drawables");
-        addDirectory(drawablesDirectory, log);
-
-        File vectorsDirectory = new File(resDirectory, "vectors");
-        addDirectory(vectorsDirectory, log);
-
-        File iconsDirectory = new File(resDirectory, "icons");
-        addDirectory(iconsDirectory, log);
-
-        //Пятый этап
-        File tempTxt = new File(tempDirectory, "temp.txt");
-        try {
-            addFile(tempTxt, log);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        try (FileWriter writer = new FileWriter(tempTxt, true)) {
+    public static void printLogsToFile (String directory, StringBuilder log) {
+        try (FileWriter writer = new FileWriter(directory, true)) {
             writer.write(String.valueOf(log));
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
-        } ;
+        }
+    }
+
+    public static void main(String[] args) {
+        StringBuilder log = new StringBuilder();
+        List<File> folderList = new ArrayList<>();
+        folderList.add(new File("E://Games/savegames"));
+        folderList.add(new File("E://Games/src/main"));
+        folderList.add(new File("E://Games/src/test"));
+        folderList.add(new File("E://Games/res/drawables"));
+        folderList.add(new File("E://Games/res/vectors"));
+        folderList.add(new File("E://Games/res/icons"));
+        folderList.add(new File("E://Games/temp/"));
+
+        List<File> filesList = new ArrayList<>();
+        filesList.add(new File("E://Games/src/main/Main.java"));
+        filesList.add(new File("E://Games/src/main/Utils.java"));
+        filesList.add(new File("E://Games/temp/temp.txt"));
+
+        addDirectory(folderList, log);
+        addFile(filesList, log);
+        printLogsToFile("E://Games/temp/temp.txt", log);
+
     }
 }
